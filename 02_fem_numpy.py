f4fd192ec5c13d78ed831c1d952384f1f5f91f29
TJUDYP <58839298+TJUDYP@users.noreply.github.com>
2022-12-04 10:04:48 +0800

    added some annotation

--- 02_fem_numpy.py
+++ 02_fem_numpy.py
@@ -5,6 +5,9 @@ from scipy.sparse.linalg import spsolve
 import matplotlib.pyplot as plt
 
 
+# ===================================================================
+# using gmsh to generate mesh in following Finete element analysis
+# ===================================================================
 gmsh.initialize()
 
 gmsh.model.add("t1")
@@ -40,15 +43,14 @@ gmsh.model.geo.addLine(8, 5, 8)
 gmsh.model.geo.addCurveLoop([5, 6, 7, 8], 2)
 
 # =========================================
-# gmsh.model.geo.addPlaneSurface([1], 1)
 gmsh.model.geo.addPlaneSurface([1, 2], 1)
-
 gmsh.model.geo.synchronize()
 gmsh.model.mesh.generate(2)
 
+# =========================================
+# get the data of triangles'tags in mesh
 entities = gmsh.model.getEntities()
 
-# get the data of triangles and coords in mesh
 for e in entities:
     if e[0] == 2:
 
@@ -58,10 +60,10 @@ for e in entities:
         # Get the mesh elements for the entity (dim, tag):
         elemTypes, elemTags, elemNodeTags = gmsh.model.mesh.getElements(dim, tag)
 
-
 element_tags = np.reshape(elemTags, np.shape(elemTags)[1])
 element_node_tags = np.reshape(elemNodeTags, (np.shape(elemTags)[1], 3))
 
+# =========================================
 # in gmsh, get all the nodes and coords if dim = -1 and tag = -1
 dim = -1
 tag = -1
@@ -70,9 +72,8 @@ nodeTags, nodeCoords, nodeParams = gmsh.model.mesh.getNodes(dim, tag)
 node_tags = nodeTags
 node_coords = np.reshape(nodeCoords, (np.shape(nodeTags)[0], 3))
 
-
+# =========================================
 # get the tags of boundary nodes
-
 nodeTags_boundary_exterior = np.array([], dtype = np.uint64)
 nodeTags_boundary_interior = np.array([], dtype = np.uint64)
 
@@ -88,18 +89,18 @@ for e in entities:
         else:
             nodeTags_boundary_interior = np.append(nodeTags_boundary_interior, np.array(nodeTags, dtype=np.uint64))
 
-
 node_tags_boundary_exterior = nodeTags_boundary_exterior
 node_tags_boundary_interior = nodeTags_boundary_interior
 node_tags_others = np.delete(node_tags, np.concatenate((node_tags_boundary_exterior-1, node_tags_boundary_interior-1)))
 
+# =========================================
 gmsh.clear()
 gmsh.finalize()
 
 
-
-
-
+# ===================================================================
+# using numpy and scipy to finish the Finete element analysis
+# ===================================================================
 def polygon_area(x, y):
     return 0.5 * np.abs(np.dot(x, np.roll(y,1))-np.dot(y, np.roll(x,1)))
 
@@ -131,13 +132,9 @@ def stiffness_assembler_2D(node_coords, element_node_tags):
     
     return A
 
-
 def mass_assembler_2D(node_coords, element_node_tags):
     number_points    = np.shape(node_coords)[0]
     number_elements  = np.shape(element_node_tags)[0]
-    
-    # sparse matrix must be used here to optimize !!!
-    # sparse matrix to get and storage data is slower :-(: csc_matrix or lil_matrix
     
     M = np.zeros((number_points, number_points))
     
@@ -178,14 +175,10 @@ def load_assembler_2D(node_coords, element_node_tags, f):
         
     return b
 
-
-
-
+# =========================================
 A = stiffness_assembler_2D(node_coords, element_node_tags)
 M = mass_assembler_2D(node_coords, element_node_tags)
 b = load_assembler_2D(node_coords, element_node_tags, f)
-
-
 
 A_00 = A[np.ix_(node_tags_others-1, node_tags_others-1)]
 A_0b = np.concatenate((A[np.ix_(node_tags_others-1, node_tags_boundary_exterior-1)], \
@@ -201,8 +194,13 @@ xi[np.concatenate((node_tags_boundary_exterior-1, node_tags_boundary_interior-1)
 
 xi[node_tags_others-1] = np.reshape(spsolve(sparse.csc_matrix(A_00), b_0-np.matmul(A_0b, b_b)), (node_tags_others.shape[0], 1))
 
+# =========================================
 plt.figure(figsize = (7.5, 6))
 plt.scatter(node_coords[:,0], node_coords[:,1], c = xi, s=10.0, cmap='viridis')
 plt.colorbar()
 plt.savefig('numpy_cal.jpg', dpi=600)
 plt.show()
+
+print("节点总数：", np.shape(node_coords)[0])    
+print("三角形单元总数：", np.shape(element_node_tags)[0])
+
